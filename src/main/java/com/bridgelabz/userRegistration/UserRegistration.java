dfe8905a8fc7c352086dc072f4a8b8c1e4cdfc6c
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-02 08:57:27 +0530

    ADDED:code to validate email id

--- src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userRegistration/UserRegistration.java
@@ -4,11 +4,18 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static String namePattern = "[A-Z][a-zA-Z]{2}[a-zA-Z]*";
+    static String emailPattern = "abc[.][a-zA-Z]+@bl[.]co[.][a-z]*";
 
-    public static void main(String[] args) {
-        System.out.println("USER REGISTRATION..");
+    public boolean checkFirstName(String firstname) {
+        boolean result = firstname.matches(namePattern);
+        return result;
     }
-    public boolean checkName(String name) {
-        return Pattern.matches(namePattern,name);
+    public boolean checkLastName(String lastName) {
+        boolean result = lastName.matches(namePattern);
+        return result;
+    }
+    public boolean checkEmailId(String emailId) {
+        boolean result = emailId.matches(emailPattern);
+        return result;
     }
 }
--- src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userRegistration/UserRegistrationTest.java
@@ -3,28 +3,27 @@ package com.bridgelabz.userRegistration;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class UserRegistrationTest {
     UserRegistration user = new UserRegistration();
     @Test
     public void givenUserFirstName_validThenTrue() {
-        boolean firstName = user.checkName("Pushkar");
-        Assert.assertTrue(firstName);
+        boolean result = user.checkFirstName("Pushkar");
+        Assert.assertTrue(result);
     }
 
-    @Test
-    public void givenUserFirstName_invalidThenFalse() {
-        boolean firstName = user.checkName("pushkar");
-        Assert.assertFalse(firstName);
-    }
     @Test
     public void givenUserLastName_validThenTrue() {
-        boolean lastName = user.checkName("Morey");
-        Assert.assertTrue(lastName);
+        boolean result = user.checkLastName("Morey");
+        Assert.assertTrue(result);
     }
 
     @Test
-    public void givenUserLastName_invalidThenFalse() {
-        boolean lastName = user.checkName("morey");
-        Assert.assertFalse(lastName);
+    public void givenEmailId_validThenTrue() {
+        boolean result = user.checkEmailId("abc.xyz@bl.co.in");
+        Assert.assertTrue(result);
     }
 }
+
+
